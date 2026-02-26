7b8435057f1bd9ba52fb31369e5b9a071ac65ca1
MamertoLuis <marty.manguerra@gmail.com>
2026-02-26 13:22:03 +0800

    cjecl

--- loader.sh
+++ loader.sh
@@ -1,4 +1,4 @@
 
-# uv run python manage.py import_borrowers /mnt/c/Users/mamer/OneDrive/Desktop/borrowers.csv
+uv run python manage.py import_borrowers /mnt/c/Users/mamer/OneDrive/Desktop/borrowers.csv
 # uv run python manage.py import_accounts /mnt/c/Users/mamer/OneDrive/Desktop/accounts.csv
 # uv run python manage.py import_exposures  /mnt/c/Users/mamer/OneDrive/Desktop/exposures.csv
